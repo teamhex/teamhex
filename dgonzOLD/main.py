9c9e7f47e7e4787f871657b19406fb093fefe885
Team HEX account <teamhex@theBestRobot.(none)>
2013-01-16 18:47:34 -0500

    waypoint

--- dgonzOLD/main.py
+++ dgonzOLD/main.py
@@ -5,6 +5,8 @@ dgonz@mit.edu
 January 2013
 """
 
+import signal
+import sys
 import motorControl as mot
 import serialComm as ser
 import waypointNav
@@ -17,62 +19,74 @@ pose = [0,0,0]
 
 def initialize():
     ser.initialize(encPort = '/dev/arduino_encoders',motPort = '/dev/arduino_control', myBaud = 1000000)
-    #waypointNav.initialize()
+    waypointNav.initialize()
     ser.sendCommand(mot.getMotorCommandBytes(0,0))
-    
+
 def update():
     global pose
-    
+
     #-------------------------Receive Data from Arduino
     # data is [Left Encoder, Right Encoder]
     data = ser.receive()
     #print data
     #-------------------------Update Odometry
     pose = odo.update(data[0],data[1])
-    
-    if((pose != odo.pose) and debug):
+
+    #    if((pose != odo.pose) and debug):
+    if debug:
         print "x = "+str(pose[0])+", y = "+str(pose[1])+", theta = "+str(math.degrees(pose[2]))
-    
+
     #-------------------------Update Waypoint Navigator
-    
-    #[forSet,angSet] = waypointNav.update(pose)
-    #mot.setAngForVels(forSet,angSet)
-    
+
+    [forSet,angSet] = waypointNav.update(pose)
+    mot.setAngForVels(forSet,angSet)
+
     #-------------------------Update Motor Controller
     [dThetaLdt, dThetaRdt] = odo.getVel()
-    
+
     [lCommand, rCommand] = mot.update(dThetaLdt, dThetaRdt)
+    #print lCommand,rCommand
     ser.sendCommand(mot.getMotorCommandBytes(lCommand,rCommand))
 
 def testVel():
     initialize()
     i = 0
     mot.setForVel(0)
+    mot.setAngVel(3)
     while(True):
-        i+=1
-        if (i/500.0== 1):
-            print "lol"
-            mot.setForVel(2)
-        elif(i/1000.0 == 1):
-            print "lol2"
-            mot.setForVel(-2)
-        elif(i/1500.0 == 1):
-            print "lol3"
-            mot.setForVel(0)
-            mot.setAngVel(.5)
-        elif(i/2000.0 == 1):
-            print "lol4"
-            mot.setAngVel(-.5)
-        elif(i/2500.0 == 1):
-            print "lol5"
-            i = 0
-            mot.setAngForVels(0,0)
+    #    i+=1
+    #    if (i/500.0== 1):
+    #        print "lol"
+    #        mot.setForVel(2)
+    #    elif(i/1000.0 == 1):
+    #        print "lol2"
+    #        mot.setForVel(-2)
+    #    elif(i/1500.0 == 1):
+    #        print "lol3"
+    #        mot.setForVel(0)
+    #        mot.setAngVel(1)
+    #    elif(i/2000.0 == 1):
+    #        print "lol4"
+    #        mot.setAngVel(-1)
+    #    elif(i/2500.0 == 1):
+    #        print "lol5"
+    #        i = 0
+    #        mot.setAngForVels(0,0)
         update()
-        
+
 def testWaypoints():
     initialize()
     waypointNav.addWaypoints([[24,0,0],[24,24,math.pi/2.0],[0,24,math.pi],[0,0,0]])
     while(True):
         update()
 
-testVel()
+def cleanQuit(signal, frame):
+    print "Interrupt received"
+    ser.sendCommand(mot.getMotorCommandBytes(0,0))
+    ser.serEnc.stop()
+    ser.serMot.stop()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, cleanQuit)
+
+testWaypoints()
--- dgonzOLD/waypointNav.py
+++ dgonzOLD/waypointNav.py
@@ -10,7 +10,7 @@ import math
 from lib.PIDControlModule import PIDController
 
 pose = [0,0,0]
-desiredPose = [0,0,0]
+desiredPose = None
 wp = [] #list of waypoints. One waypoint is of form [x, y, theta] in [inches, inches, radians]
 
 #Finite State Machine Setup
@@ -20,80 +20,86 @@ TRANSLATING = 2
 
 state = IDLE
 
-angTroller = PIDController(.8,.01,0)
-transTroller = PIDController(.8,.01,0)
+angTroller = PIDController(1,.0001,.001)
+transTroller = PIDController(-.1,-.0001,-0.001)
 
 def initialize():
     print "waypoint Navigator initialized"
     return
-    
+
 def dist(x1,y1,x2,y2):
     return math.sqrt((x1-x2)**2+(y1-y2)**2)
 
 def getAngle(x1,y1,x2,y2):
     return math.atan2(y2-y1,x2-x1)
-    
+
 def angDiff(ang1,ang2):
-    #TODO: Implement
-    diff = ang2-ang1
-    return diff
-    
+    diff1 = (ang2-ang1)%(2*math.pi)
+    diff2 = (ang1-ang2)%(2*math.pi)
+    if diff1 < diff2:
+        return -diff1
+    else:
+        return diff2
+
 def compareAng(ang1,ang2):
     #TODO: fix the angles, right now robot will "unwind"
-    eTheta = 2.0*math.pi/500
-    if(angDiff(ang1,ang2)<eTheta):
+    eTheta = 2.0*math.pi/500.0
+    if(abs(angDiff(ang1,ang2))<eTheta):
         return True
     else:
         return False
 
 def compareDist(pose1,pose2):
-    eXY = .0625
+    eXY = .25
     if(dist(pose1[0],pose1[1],pose2[0],pose2[1])<eXY):
         return True
     else:
         return False
-    
+
 def comparePose(pose1, pose2):
-    eXY = .0625
-    eTheta = 2.0*math.pi/1000
-    if(compareDist(pose1,pose2) and compareAng(pose1[2],pose2[2])):
+    if(compareDist(pose1,pose2)):# and compareAng(pose1[2],pose2[2])):
         return True
     else:
         return False
-    
+
 def update(myPose):
     """returns a vector [ForwardVel,AngularVel]"""
     global wp
     global pose
     global desiredPose
+    global desiredAngle
     global state
-    
+
+    desiredPose = wp[0]
+
     pose = myPose
-    
+    desiredAngle = getAngle(pose[0],pose[1],desiredPose[0],desiredPose[1])
+    #print angDiff(pose[2],desiredAngle)
+
     if(state == IDLE):
         print "Sup"
         if(not comparePose(pose,desiredPose)):
             state = ROTATING
             print "WPState = ROTATE"
-            angTroller.setDesired(desiredPose[2],reset = True)
+            angTroller.setDesired(0,reset = True)
         return [0,0]
     elif(state == ROTATING):
-        if(compareAng(pose[0],desiredPose[1])):
+        if(compareAng(pose[2],desiredAngle)):
             state = TRANSLATING
             print "WPState = TRANSLATING"
             transTroller.setDesired(0,reset = True)
             return [0,0]
         else:
-            return [0,angTroller.update(pose[2])]
+            return [0,angTroller.update(angDiff(pose[2],desiredAngle))]
     elif(state == TRANSLATING):
         if(comparePose(pose,desiredPose)):
             if(len(wp)==0):
                 state= IDLE
-                
+
                 print "WPState = IDLE"
                 return [0,0]
             else:
-                desiredPose = wp.pop(0)
+                wp.pop(0)
                 state = IDLE
                 return [0,0]
         else:
@@ -102,6 +108,6 @@ def update(myPose):
 def addWaypoint(newWP):
     global wp
     wp.append(newWP)
-    
+
 def addWaypoints(newWPs):
     [addWaypoint(x) for x in newWPs]
